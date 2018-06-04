a5619c335926fbcf4e649cd5f4f10d335bf5119e
galihsatriawan <satriawan.galih99@gmail.com>
2018-06-04 09:13:12 +0700

    Penambahan dictionary dan ArrayList of GrapNode

--- Algorithm Strategy/src/algorithm/strategy/AlgorithmStrategy.java
+++ Algorithm Strategy/src/algorithm/strategy/AlgorithmStrategy.java
@@ -5,6 +5,9 @@
  */
 package algorithm.strategy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author root
@@ -14,6 +17,17 @@ public class AlgorithmStrategy {
     /**
      * @param args the command line arguments
      */
+    /*
+        Dictionary Node :
+        - dictionary disini digunakan untuk mempermudah mencari suatu node 
+        dengan menggunakan string atau nama dari node tersebut
+    */ 
+    
+    public HashMap<String,GraphNode> dict_node = new HashMap();
+    
+    //Kumpulan Node
+    public ArrayList<GraphNode> kump_node = new ArrayList<>();
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
