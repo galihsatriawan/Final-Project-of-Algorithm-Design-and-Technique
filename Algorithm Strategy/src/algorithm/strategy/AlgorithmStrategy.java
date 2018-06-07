d8ea8367866ed4fb282b6b596ac9eb446c23e540
LENOVO-PC\rhesal <rhesal.mahadyanto@gmail.com>
2018-06-07 12:05:37 +0700

    Perbaikan insertKump_Node dan penambahan sample node

--- Algorithm Strategy/src/algorithm/strategy/AlgorithmStrategy.java
+++ Algorithm Strategy/src/algorithm/strategy/AlgorithmStrategy.java
@@ -23,22 +23,37 @@ public class AlgorithmStrategy {
         dengan menggunakan string atau nama dari node tersebut
     */ 
     
-    public HashMap<String,GraphNode> dict_node = new HashMap();
+    public static HashMap<String,GraphNode> dict_node = new HashMap();
     
     //Kumpulan Node
-    public ArrayList<GraphNode> kump_node = new ArrayList<>();
+    public static ArrayList<GraphNode> kump_node = new ArrayList<>();
     
     // insert node
-    void insertKump_Node(GraphNode new_node){
-        this.kump_node.add(new_node);
+    static void insertKump_Node(GraphNode new_node){
+        kump_node.add(new_node);
+        insertDict_Node(new_node.getNama(), new_node);
     }
     
-    void insertDict_Node(String new_key, GraphNode new_node){
-        this.dict_node.put(new_key, new_node);
+    static void insertDict_Node(String new_key, GraphNode new_node){
+        dict_node.put(new_key, new_node);
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
+        //Sample Insert Node 
+        GraphNode a = new GraphNode(0, "Malang");
+        insertKump_Node(a);
+        
+        GraphNode b = new GraphNode(1, "Batu");
+        insertKump_Node(b);
+        
+        GraphNode c = new GraphNode(2, "Kab Malang");
+        insertKump_Node(c);
+        
+        GraphNode d = new GraphNode(3, "Kepanjen");
+        insertKump_Node(d);
+        
+        
     }
     
 }
